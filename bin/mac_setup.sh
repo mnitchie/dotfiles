7cdc195c7ba428bf6f57cae365270403a6827304
Mike Nitchie <mikenitchie@gmail.com>
2023-09-02 19:40:42 -0400

    More setup

--- bin/mac_setup.sh
+++ bin/mac_setup.sh
@@ -4,3 +4,13 @@ echo "Installing brew"
 echo "Installing 1password cli. Remember to enable this in the 1password desktop settings"
 brew install 1password-cli
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $XDG_CONFIG_HOME/zsh/.zprofile
+
+echo "Installing font needed for agnoster theme"
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
