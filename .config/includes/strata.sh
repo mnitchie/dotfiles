06fca40746c7a3a073adf50a90b1d3ddb32c0b6f
Mike Nitchie <mikenitchie@gmail.com>
2025-10-14 17:11:27 -0400

    remove vpn cruft

--- .config/includes/strata.sh
+++
@@ -1,7 +0,0 @@
-# Strata VPN
-alias vpn="/opt/cisco/secureclient/bin/vpn"
-
-function vpnup {
-	PSWD=$(security find-generic-password -a ${USER} -s stratavpn -w)
-	printf 'michael.nitchie\n%s\npush' "$PSWD" | vpn -s connect sslvpn.strataoncology.com
-}
